38920c11164f3637506f25eb03b31c9aae29a4ee
Cody <J.yongyao11@gmail.com>
2016-05-30 15:10:46 -0400

    Added an example

---
+++ src/main/java/esiptestbed/mudrod/metadata/pre/Example.java
@@ -0,0 +1,35 @@
+package esiptestbed.mudrod.metadata.pre;
+
+import java.util.Map;
+
+import esiptestbed.mudrod.discoveryengine.DiscoveryStepAbstract;
+import esiptestbed.mudrod.driver.ESDriver;
+
+/**
+ * Say sth
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Example extends DiscoveryStepAbstract {
+
+	public Example(Map<String, String> config, ESDriver es) {
+		super(config, es);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		System.out.println("*****************Step 1: Example******************");
+		startTime=System.currentTimeMillis();
+		es.createBulkProcesser();
+        /* Do something */
+		es.destroyBulkProcessor();
+		endTime=System.currentTimeMillis();
+		System.out.println("*****************Example ends******************Took " + (endTime-startTime)/1000+"s");
+	}
+
+}
