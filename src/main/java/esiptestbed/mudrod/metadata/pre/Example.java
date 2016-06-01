d182c85d1ea7aa3f9fd825e77545a2c88e584985
Yongyao Jiang <J.yongyao11@gmail.com>
2016-06-01 10:40:23 -0400

    test commit

--- src/main/java/esiptestbed/mudrod/metadata/pre/Example.java
+++ src/main/java/esiptestbed/mudrod/metadata/pre/Example.java
@@ -40,7 +40,7 @@ public class Example extends DiscoveryStepAbstract {
     startTime=System.currentTimeMillis();
     es.createBulkProcesser();
     /* Do something */
-
+    //test
     es.destroyBulkProcessor();
     endTime=System.currentTimeMillis();
     System.out.println("*****************Example ends******************Took " + (endTime-startTime)/1000+"s");
