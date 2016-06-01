ad9b7339890ae8d05ff3dfeb60b43e540b264bfd
Cody <J.yongyao11@gmail.com>
2016-05-31 22:22:48 -0400

    Import Mudrod abstract to discoveryengineabstract

--- src/main/java/esiptestbed/mudrod/discoveryengine/DiscoveryEngineAbstract.java
+++ src/main/java/esiptestbed/mudrod/discoveryengine/DiscoveryEngineAbstract.java
@@ -4,12 +4,15 @@ import java.util.Map;
 
 import esiptestbed.mudrod.driver.ESDriver;
 
-public abstract class DiscoveryEngineAbstract extends MudrodAbstract{
+public abstract class DiscoveryEngineAbstract extends MudrodAbstract {
+
 	public DiscoveryEngineAbstract(Map<String, String> config, ESDriver es) {
 		super(config, es);
 		// TODO Auto-generated constructor stub
 	}
+	
 	public abstract void preprocess();
 	public abstract void process();
 	public abstract void output();
+
 }
