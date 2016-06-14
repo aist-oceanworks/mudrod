8b1243f575fda41b6bae13652ea6ce9ae7c856a9
Yongyao Jiang <J.yongyao11@gmail.com>
2016-06-14 11:14:27 -0400

    remaining changes

--- src/main/java/esiptestbed/mudrod/discoveryengine/MudrodAbstract.java
+++ src/main/java/esiptestbed/mudrod/discoveryengine/MudrodAbstract.java
@@ -29,54 +29,54 @@ import esiptestbed.mudrod.driver.SparkDriver;
  * 
  */
 public abstract class MudrodAbstract implements Serializable {
-  protected Map<String, String> config = new HashMap<String, String>();
-  protected ESDriver es = null;
+	protected Map<String, String> config = new HashMap<String, String>();
+	protected ESDriver es = null;
 	protected SparkDriver spark = null;
-  protected long startTime, endTime;
-  public String HTTP_type = null;
-  public String FTP_type = null;
-  public String Cleanup_type = null;
-  public String SessionStats = null;
-  public final String settings_json = "{\r\n      \"analysis\": {\r\n         \"filter\": {\r\n            \"cody_stop\": {\r\n               \"type\": \"stop\",\r\n               \"stopwords\": \"_english_\"\r\n            },\r\n            \"cody_stemmer\": {\r\n               \"type\": \"stemmer\",\r\n               \"language\": \"light_english\"\r\n            }\r\n         },\r\n         \"analyzer\": {\r\n            \"cody\": {\r\n               \"tokenizer\": \"standard\",\r\n               \"filter\": [\r\n                  \"lowercase\",\r\n                  \"cody_stop\",\r\n                  \"cody_stemmer\"\r\n               ]\r\n            },\r\n            \"csv\": {\r\n               \"type\": \"pattern\",\r\n               \"pattern\": \",\"\r\n            }\r\n         }\r\n      }\r\n   }";
-  public final String mapping_json = "{\r\n      \"_default_\": {\r\n         \"properties\": {\r\n            \"keywords\": {\r\n               \"type\": \"string\",\r\n               \"index_analyzer\": \"csv\"\r\n            },\r\n            \"views\": {\r\n               \"type\": \"string\",\r\n               \"index_analyzer\": \"csv\"\r\n            },\r\n            \"downloads\": {\r\n               \"type\": \"string\",\r\n               \"index_analyzer\": \"csv\"\r\n            },\r\n            \"RequestUrl\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"IP\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Browser\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"SessionURL\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Referer\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"SessionID\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Response\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Request\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Coordinates\": {\r\n               \"type\": \"geo_point\"\r\n            }\r\n         }\r\n      }\r\n   }";
+	protected long startTime, endTime;
+	public String HTTP_type = null;
+	public String FTP_type = null;
+	public String Cleanup_type = null;
+	public String SessionStats = null;
+	public final String settings_json = "{\r\n      \"analysis\": {\r\n         \"filter\": {\r\n            \"cody_stop\": {\r\n               \"type\": \"stop\",\r\n               \"stopwords\": \"_english_\"\r\n            },\r\n            \"cody_stemmer\": {\r\n               \"type\": \"stemmer\",\r\n               \"language\": \"light_english\"\r\n            }\r\n         },\r\n         \"analyzer\": {\r\n            \"cody\": {\r\n               \"tokenizer\": \"standard\",\r\n               \"filter\": [\r\n                  \"lowercase\",\r\n                  \"cody_stop\",\r\n                  \"cody_stemmer\"\r\n               ]\r\n            },\r\n            \"csv\": {\r\n               \"type\": \"pattern\",\r\n               \"pattern\": \",\"\r\n            }\r\n         }\r\n      }\r\n   }";
+	public final String mapping_json = "{\r\n      \"_default_\": {\r\n         \"properties\": {\r\n            \"keywords\": {\r\n               \"type\": \"string\",\r\n               \"index_analyzer\": \"csv\"\r\n            },\r\n            \"views\": {\r\n               \"type\": \"string\",\r\n               \"index_analyzer\": \"csv\"\r\n            },\r\n            \"downloads\": {\r\n               \"type\": \"string\",\r\n               \"index_analyzer\": \"csv\"\r\n            },\r\n            \"RequestUrl\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"IP\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Browser\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"SessionURL\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Referer\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"SessionID\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Response\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Request\": {\r\n               \"type\": \"string\",\r\n               \"index\": \"not_analyzed\"\r\n            },\r\n            \"Coordinates\": {\r\n               \"type\": \"geo_point\"\r\n            }\r\n         }\r\n      }\r\n   }";
 
-  private int print_level = 0;
+	private int print_level = 0;
 
 	public MudrodAbstract(Map<String, String> config, ESDriver es, SparkDriver spark){
-    this.config = config;
+		this.config = config;
 		this.es = es;
 		this.spark = spark;
 		this.initMudrod();
 	}
-	
+
 	protected void initMudrod(){
 		try {
 			es.putMapping(config.get("indexName"), settings_json, mapping_json);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    HTTP_type = config.get("HTTP_type");
-    FTP_type = config.get("FTP_type");
-    Cleanup_type = config.get("Cleanup_type");
-    SessionStats = config.get("SessionStats");
+		HTTP_type = config.get("HTTP_type");
+		FTP_type = config.get("FTP_type");
+		Cleanup_type = config.get("Cleanup_type");
+		SessionStats = config.get("SessionStats");
 
-    print_level = Integer.parseInt(config.get("loglevel"));
-  }
+		print_level = Integer.parseInt(config.get("loglevel"));
+	}
 
-  public void print(String log, int level){
-    if(level <= print_level)
-    {
-      System.out.println(log);
-    }
-  }
+	public void print(String log, int level){
+		if(level <= print_level)
+		{
+			System.out.println(log);
+		}
+	}
 
-  public ESDriver getES(){
-    return this.es;
-  }
+	public ESDriver getES(){
+		return this.es;
+	}
 
-  public Map<String, String> getConfig(){
-    return this.config;
-  }
+	public Map<String, String> getConfig(){
+		return this.config;
+	}
 }
