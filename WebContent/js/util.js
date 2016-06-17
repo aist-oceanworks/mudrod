fdee41eaeeb1b8a0ed733378453a7544d2a578cd
Yongyao Jiang <J.yongyao11@gmail.com>
2016-06-17 11:25:25 -0400

    test

--- WebContent/js/util.js
+++ WebContent/js/util.js
@@ -1,31 +1,43 @@
-$(document).ready(function(){			
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+$(document).ready(function(){
 
 });
 
-
 function setGetParameter(paramName, paramValue)
 {
-		var url = window.location.href;
-		var hash = location.hash;
-		url = url.replace(hash, '');
-		if (url.indexOf(paramName + "=") >= 0)
-		{
-			var prefix = url.substring(0, url.indexOf(paramName));
-			var suffix = url.substring(url.indexOf(paramName));
-			suffix = suffix.substring(suffix.indexOf("=") + 1);
-			suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
-			url = prefix + paramName + "=" + paramValue + suffix;
-		}
-		else
-		{
+	var url = window.location.href;
+	var hash = location.hash;
+	url = url.replace(hash, '');
+	if (url.indexOf(paramName + "=") >= 0)
+	{
+		var prefix = url.substring(0, url.indexOf(paramName));
+		var suffix = url.substring(url.indexOf(paramName));
+		suffix = suffix.substring(suffix.indexOf("=") + 1);
+		suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
+		url = prefix + paramName + "=" + paramValue + suffix;
+	}
+	else
+	{
 		if (url.indexOf("?") < 0)
 			url += "?" + paramName + "=" + paramValue;
 		else
 			url += "&" + paramName + "=" + paramValue;
-		}
-		window.location.href = url + hash;
+	}
+	window.location.href = url + hash;
 }
-	
+
 function getURLParameter(name) {
-	  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+	return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
