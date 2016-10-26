620120a3fed7c6ca45cf6e49157d3d510e0e728c
YunLi <tina.yuner@gmail.com>
2016-10-26 13:07:30 -0700

    clean code and add comments

--- core/src/main/java/esiptestbed/mudrod/weblog/structure/ApacheAccessLog.java
+++ core/src/main/java/esiptestbed/mudrod/weblog/structure/ApacheAccessLog.java
@@ -44,66 +44,6 @@ public class ApacheAccessLog extends WebLog implements Serializable {
 
   }
 
-  /* public static ApacheAccessLog parseFromLogLine(String log)
-      throws IOException, ParseException {
-  
-    String logEntryPattern = "^([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+|-) \"((?:[^\"]|\")+)\" \"([^\"]+)\"";
-    final int NUM_FIELDS = 9;
-    Pattern p = Pattern.compile(logEntryPattern);
-    Matcher matcher;
-  
-    String lineJson = "{}";
-    matcher = p.matcher(log);
-    if (!matcher.matches() || NUM_FIELDS != matcher.groupCount()) {
-      return null;
-    }
-  
-    String time = matcher.group(4);
-    time = SwithtoNum(time);
-    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss");
-    Date date = formatter.parse(time);
-  
-    String bytes = matcher.group(7);
-  
-    if (bytes.equals("-")) {
-      bytes = "0";
-    }
-  
-    String request = matcher.group(5).toLowerCase();
-    String agent = matcher.group(9);
-    CrawlerDetection crawlerDe = new CrawlerDetection();
-    if (crawlerDe.checkKnownCrawler(agent)) {
-      return null;
-    } else {
-  
-      boolean tag = false;
-      String[] mimeTypes = { ".js", ".css", ".jpg", ".png", ".ico",
-          "image_captcha", "autocomplete", ".gif", "/alldata/", "/api/",
-          "get / http/1.1", ".jpeg", "/ws/" };
-      for (int i = 0; i < mimeTypes.length; i++) {
-        if (request.contains(mimeTypes[i])) {
-          tag = true;
-          return null;
-        }
-      }
-  
-      if (tag == false) {
-        ApacheAccessLog accesslog = new ApacheAccessLog();
-        accesslog.LogType = "PO.DAAC";
-        accesslog.IP = matcher.group(1);
-        accesslog.Time = date;
-        accesslog.Request = matcher.group(5);
-        accesslog.Response = matcher.group(6);
-        accesslog.Bytes = Double.parseDouble(bytes);
-        accesslog.Referer = matcher.group(8);
-        accesslog.Browser = matcher.group(9);
-        return accesslog;
-      }
-    }
-  
-    return null;
-  }*/
-
   public static String parseFromLogLine(String log)
       throws IOException, ParseException {
 
--- core/src/main/java/esiptestbed/mudrod/weblog/structure/FtpLog.java
+++ core/src/main/java/esiptestbed/mudrod/weblog/structure/FtpLog.java
@@ -14,39 +14,6 @@ import com.google.gson.Gson;
  */
 public class FtpLog extends WebLog implements Serializable {
 
-  /* public static FtpLog parseFromLogLine(String log)
-      throws IOException, ParseException {
-  
-    String ip = log.split(" +")[6];
-  
-    String time = log.split(" +")[1] + ":" + log.split(" +")[2] + ":"
-        + log.split(" +")[3] + ":" + log.split(" +")[4];
-  
-    time = SwithtoNum(time);
-    SimpleDateFormat formatter = new SimpleDateFormat("MM:dd:HH:mm:ss:yyyy");
-    Date date = formatter.parse(time);
-    String bytes = log.split(" +")[7];
-  
-    String request = log.split(" +")[8].toLowerCase();
-  
-    if (!request.contains("/misc/") && !request.contains("readme")) {
-      FtpLog ftplog = new FtpLog();
-      ftplog.LogType = "ftp";
-      ftplog.IP = ip;
-      ftplog.Request = request;
-      ftplog.Bytes = Double.parseDouble(bytes);
-  
-      SimpleDateFormat df = new SimpleDateFormat(
-          "yyyy-MM-dd'T'HH:mm:ss.sss'Z'");
-      ftplog.Time = df.format(date);
-      // ftplog.Time = date;
-  
-      return ftplog;
-    }
-  
-    return null;
-  }*/
-
   public static String parseFromLogLine(String log)
       throws IOException, ParseException {
 
