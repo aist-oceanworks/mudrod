d721c1e2a7f9157efa93a2e13710c368bc7b48dd
Frank Greguska <francis.greguska@jpl.nasa.gov>
2017-03-02 13:52:19 -0800

    formatting

--- web/META-INF/resources/js/search.js
+++ web/META-INF/resources/js/search.js
@@ -13,166 +13,166 @@
  */
 var g_currentQuery, g_currentSearchOption;
 
-$(document).ready(function() {
-	var query = getURLParameter('query');
-	g_currentSearchOption = getURLParameter('searchOption');
-	if(g_currentSearchOption) {
-		$('#searchOption_' + g_currentSearchOption).prop('checked', true);
-	}
-	if (query == null) {
-		$("#searchResults").hide();
-	} else {
-		g_currentQuery = encodeURI(query);
-		$("#searchResults").hide();
-		$("#NotFound").hide();
-		$("#query").val(query);
-		search(query);
-	}
+$(document).ready(function () {
+    var query = getURLParameter('query');
+    g_currentSearchOption = getURLParameter('searchOption');
+    if (g_currentSearchOption) {
+        $('#searchOption_' + g_currentSearchOption).prop('checked', true);
+    }
+    if (query == null) {
+        $("#searchResults").hide();
+    } else {
+        g_currentQuery = encodeURI(query);
+        $("#searchResults").hide();
+        $("#NotFound").hide();
+        $("#query").val(query);
+        search(query);
+    }
 
-	$("#query").keyup(function(event) {
-		if (event.keyCode == 13) {
-			$("#searchButton").click();
-		}
-	});
+    $("#query").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#searchButton").click();
+        }
+    });
 
-	$("#searchButton").click(function() {
-		redirect("search", "query", $("#query").val(), "searchOption", $("input[name='searchOption']:checked").val());
-	});
+    $("#searchButton").click(function () {
+        redirect("search", "query", $("#query").val(), "searchOption", $("input[name='searchOption']:checked").val());
+    });
 
-	$("#ontologyUL").on("click", "li a", function(){
-		redirect("search", "query", $(this).data("word"), "searchOption", $("input[name='searchOption']:checked").val());
-	});
+    $("#ontologyUL").on("click", "li a", function () {
+        redirect("search", "query", $(this).data("word"), "searchOption", $("input[name='searchOption']:checked").val());
+    });
 });
 
 function search(query) {
-	if ($("#query").val() != "") {
-		$("#searchBox").append($("#searchGroup"));
-		$("#searchjumbo").hide();
-		$("#note").hide();
-		$("#searchResults").show();
-		$("#searchLoading").show();
+    if ($("#query").val() != "") {
+        $("#searchBox").append($("#searchGroup"));
+        $("#searchjumbo").hide();
+        $("#note").hide();
+        $("#searchResults").show();
+        $("#searchLoading").show();
 
-		$("#searchContainer").css("margin-top", "30px");
-		$("#searchResultContainer").show();
-		$("#searchContainer h2.title").css("font-size", "24px");
-		var search_operator = $("input[name='searchOption']:checked").val();
-		
-		// invoke SearchMetadataResource.java
-		$.ajax({
-			url : "services/metadata/search",
-			data : {
-				"query" : $("#query").val(),
-				"operator": $("input[name='searchOption']:checked").val()
-			},
-			success : function completeHandler(response) {
-				if (response != null) {
-					response = JSON.parse(response)
-					$("#searchLoading").hide();
-					var searchResults = response.PDResults;
-					if (searchResults.length == 0) {
-						$("#NotFound").show();
-						$("#searchKeyword").html($("#query").val());
-						$("#resultCount, #ontology-results").hide();
-					} else {
-						$("#NotFound").hide();
-						$("#resultCount, #ontology-results").show();
-						$("#resultCount").html(searchResults.length + ' matches');
-						createResultTable();
-						$('#ResultsTable').bootstrapTable('load', searchResults);
-					}
-				}
-			}
-		});
+        $("#searchContainer").css("margin-top", "30px");
+        $("#searchResultContainer").show();
+        $("#searchContainer h2.title").css("font-size", "24px");
+        var search_operator = $("input[name='searchOption']:checked").val();
 
-		// invoke SearchVocabResource.java
-		$.ajax({
-			url: "services/vocabulary/search",
-			data: {
-				"concept": $("#query").val().toLowerCase(),
-				"operator": $("input[name='searchOption']:checked").val()
-			},
-			success: function completeHandler(response) {
-				if (response != null) {
+        // invoke SearchMetadataResource.java
+        $.ajax({
+            url: "services/metadata/search",
+            data: {
+                "query": $("#query").val(),
+                "operator": $("input[name='searchOption']:checked").val()
+            },
+            success: function completeHandler(response) {
+                if (response != null) {
+                    response = JSON.parse(response)
+                    $("#searchLoading").hide();
+                    var searchResults = response.PDResults;
+                    if (searchResults.length == 0) {
+                        $("#NotFound").show();
+                        $("#searchKeyword").html($("#query").val());
+                        $("#resultCount, #ontology-results").hide();
+                    } else {
+                        $("#NotFound").hide();
+                        $("#resultCount, #ontology-results").show();
+                        $("#resultCount").html(searchResults.length + ' matches');
+                        createResultTable();
+                        $('#ResultsTable').bootstrapTable('load', searchResults);
+                    }
+                }
+            }
+        });
+
+        // invoke SearchVocabResource.java
+        $.ajax({
+            url: "services/vocabulary/search",
+            data: {
+                "concept": $("#query").val().toLowerCase(),
+                "operator": $("input[name='searchOption']:checked").val()
+            },
+            success: function completeHandler(response) {
+                if (response != null) {
                     JSON.parse(response)
-					var ontologyResults = response.graph.ontology;
-					if(ontologyResults.length == 0) {
-						//TODO return a simple JSON response indicating no hits.
-					} else {
-						for(var i = 0; i < ontologyResults.length; i++){
-							$("#ontologyUL").append("<li><a data-word='" + ontologyResults[i].word  + "' href='#'>" + ontologyResults[i].word + " (" + ontologyResults[i].weight + ")</a></li>");
-						}
-					}
-				}
-			}
-		});
+                    var ontologyResults = response.graph.ontology;
+                    if (ontologyResults.length == 0) {
+                        //TODO return a simple JSON response indicating no hits.
+                    } else {
+                        for (var i = 0; i < ontologyResults.length; i++) {
+                            $("#ontologyUL").append("<li><a data-word='" + ontologyResults[i].word + "' href='#'>" + ontologyResults[i].word + " (" + ontologyResults[i].weight + ")</a></li>");
+                        }
+                    }
+                }
+            }
+        });
 
-		// invoke OntologyResource.java synonym search
-		$.ajax({
-			url: "services/ontology/synonym",
-			data : {
-				"query" : $("#query").val(),
-				"operator": $("input[name='searchOption']:checked").val()
-			},
-			success: function completeHandler(response) {
-				if (response != null) {
-					//TODO
-				}
-			}
-		});
+        // invoke OntologyResource.java synonym search
+        $.ajax({
+            url: "services/ontology/synonym",
+            data: {
+                "query": $("#query").val(),
+                "operator": $("input[name='searchOption']:checked").val()
+            },
+            success: function completeHandler(response) {
+                if (response != null) {
+                    //TODO
+                }
+            }
+        });
 
-		// invoke OntologyResource.java subclass search
-		$.ajax({
-			url: "services/ontology/subclass",
-			data : {
-				"query" : $("#query").val(),
-				"operator": $("input[name='searchOption']:checked").val()
-			},
-			success: function completeHandler(response) {
-				if (response != null) {
-					//TODO
-				}
-			}
-		});
-	}
+        // invoke OntologyResource.java subclass search
+        $.ajax({
+            url: "services/ontology/subclass",
+            data: {
+                "query": $("#query").val(),
+                "operator": $("input[name='searchOption']:checked").val()
+            },
+            success: function completeHandler(response) {
+                if (response != null) {
+                    //TODO
+                }
+            }
+        });
+    }
 }
 
 function FileNameFormatter(value) {
-	var url = "http://podaac.jpl.nasa.gov/ws/metadata/dataset?format=gcmd&shortName="
-		+ encodeURIComponent(value);
-	url = "./dataset.html?query=" + g_currentQuery + "&searchOption=" + g_currentSearchOption + "&shortname=" + value;
-	return '<a class="fileShortName" href=' + url + ' target="_blank">' + value + '</a>';
+    var url = "http://podaac.jpl.nasa.gov/ws/metadata/dataset?format=gcmd&shortName="
+        + encodeURIComponent(value);
+    url = "./dataset.html?query=" + g_currentQuery + "&searchOption=" + g_currentSearchOption + "&shortname=" + value;
+    return '<a class="fileShortName" href=' + url + ' target="_blank">' + value + '</a>';
 }
 
 function createResultTable() {
-	var layout = {
-			cache : false,
-			pagination : true,
-			pageSize : 10,
-			striped : true,
-			cardView : true,
-			showHeader : true,
+    var layout = {
+        cache: false,
+        pagination: true,
+        pageSize: 10,
+        striped: true,
+        cardView: true,
+        showHeader: true,
 
-			columns : [ {
-				'title' : 'Short Name',
-				'field' : 'Short Name',
-				'formatter' : FileNameFormatter,
-				sortable : true
-			}, {
-				'title' : 'Long Name',
-				'field' : 'Long Name',
-			}, {
-				'title' : 'Topic',
-				'field' : 'Topic',
-				'formatter' : TopicFormatter,
-			}, {
-				'title' : 'Release Date',
-				'field' : 'Release Date',
-			}, {
-				'title' : 'Abstract',
-				'field' : 'Abstract',
-			} ]
+        columns: [{
+            'title': 'Short Name',
+            'field': 'Short Name',
+            'formatter': FileNameFormatter,
+            sortable: true
+        }, {
+            'title': 'Long Name',
+            'field': 'Long Name',
+        }, {
+            'title': 'Topic',
+            'field': 'Topic',
+            'formatter': TopicFormatter,
+        }, {
+            'title': 'Release Date',
+            'field': 'Release Date',
+        }, {
+            'title': 'Abstract',
+            'field': 'Abstract',
+        }]
 
-	};
+    };
 
-	$('#ResultsTable').bootstrapTable(layout);
+    $('#ResultsTable').bootstrapTable(layout);
 }
