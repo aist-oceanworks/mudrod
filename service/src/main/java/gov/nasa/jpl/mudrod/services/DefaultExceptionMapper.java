64ff9f273ff08eb4c205ba210a4886eeb83b6655
Frank Greguska <francis.greguska@jpl.nasa.gov>
2017-04-11 16:07:17 -0700

    added default exception mapper so we dont puke out server information when a request fails

---
+++ service/src/main/java/gov/nasa/jpl/mudrod/services/DefaultExceptionMapper.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gov.nasa.jpl.mudrod.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.UUID;
+
+/**
+ * Created by greguska on 4/11/17.
+ */
+@Provider
+public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(DefaultExceptionMapper.class);
+
+  @Override
+  @Produces("text/html")
+  public Response toResponse(Exception e) {
+
+    UUID errorId = UUID.randomUUID();
+    LOG.error("Internal server error " + errorId.toString(), e);
+
+    return Response.serverError()
+        .entity("<p>An error occurred while processing your request. Please contact the system administrator and provide the following error log ID " + errorId.toString() + "</p>").build();
+  }
+}
