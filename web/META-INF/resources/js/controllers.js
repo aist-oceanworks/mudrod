3880b253d1e38f288465f8dfe9a242f50e4073ae
Frank Greguska <francis.greguska@jpl.nasa.gov>
2017-06-06 18:32:59 -0700

    fixed search options on search page

--- web/META-INF/resources/js/controllers.js
+++ web/META-INF/resources/js/controllers.js
@@ -17,75 +17,62 @@
 /* Controllers */
 var mudrodControllers = angular.module('mudrodControllers', []);
 
-mudrodControllers.controller('searchCtrl', ['$scope', '$rootScope', '$location', 'Autocomplete', 'SearchOptions', 
+mudrodControllers.controller('searchCtrl', ['$scope', '$rootScope', '$location', 'Autocomplete', 'SearchOptions',
     function ($scope, $rootScope, $location, Autocomplete, SearchOptions) {
         $scope.options = {
             opt: 'Or'
         };
 
-        //if ($rootScope.SearchOptions.opt != null)
-        //    $scope.options.opt = $rootScope.searchOptions.opt;
-
-        //$scope.relatedTerms = [];
-        //$rootScope.searchOptions = {};
-        
-        $scope.complete = function(string) {  
-            $scope.hidethis = false;  
-            $scope.hideoption = false;
-            var output = [];  
+        $scope.complete = function (string) {
+            $scope.hidethis = false;
             Autocomplete.get({term: string},
                 function success(response) {
-                    //alert($scope.challenge.question);
-                    //console.log("Success:" + JSON.stringify(response));
-                    //$scope.blogList = response;
                     $scope.filterSearch = response;
 
                 },
                 function error(errorResponse) {
                     console.log("Error:" + JSON.stringify(errorResponse));
                 }
-            );  
-        }  
+            );
+        };
 
-        $scope.fillTextbox = function(string){  
-            $scope.options.query = string;  
-            $scope.hidethis = true;  
-            $scope.hideoption = false;
-        }  
+        $scope.fillTextbox = function (string) {
+            $scope.options.query = string;
+            $scope.hidethis = true;
+        };
 
-        $scope.search = function(options) {
+        $scope.search = function (options) {
             $rootScope.searchOptions = angular.copy(options);
             $location.path("/metadataView/" + options.query + '/' + options.opt);
         };
 
-        $scope.$watch(function () { return SearchOptions.getSearchOptions(); }, function (newValue, oldValue) {
-            if (newValue != null) {
-                $scope.options.query= newValue.query;
-                $scope.options.opt= newValue.opt;
+        $scope.$watch(function () {
+            return SearchOptions.getSearchOptions();
+        }, function (newValue, oldValue) {
+            if (newValue !== null) {
+                $scope.options.query = newValue.query;
+                $scope.options.opt = newValue.opt;
             }
         }, true);
     }]);
-    
+
 mudrodControllers.controller('vocabularyCtrl', ['$scope', '$rootScope', 'VocabList',
     function vocabularyCtrl($scope, $rootScope, VocabList) {
 
         var word = $rootScope.searchOptions.query;
         var opt = $rootScope.searchOptions.opt;
         VocabList.get({query: word},
-                function success(response) {
-                    //alert($scope.challenge.question);
-                    //console.log("Success:" + JSON.stringify(response));
-                    //$scope.blogList = response;
-                    $scope.ontologyList = response.graph.ontology;
+            function success(response) {
+                $scope.ontologyList = response.graph.ontology;
 
-                },
-                function error(errorResponse) {
-                    console.log("Error:" + JSON.stringify(errorResponse));
-                }
+            },
+            function error(errorResponse) {
+                console.log("Error:" + JSON.stringify(errorResponse));
+            }
         );
     }]);
 
-mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$routeParams', 'MetaData', 'PagerService', 'SearchOptions', 
+mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$routeParams', 'MetaData', 'PagerService', 'SearchOptions',
     function metadataViewCtrl($rootScope, $scope, $routeParams, MetaData, PagerService, SearchOptions) {
         var vm = this;
         vm.PDItems = [];
@@ -93,20 +80,20 @@ mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$rout
         vm.setPage = setPage;
         vm.totalMatches = 0;
 
-        var word = new String();
-        var opt = new String();
-        
-        if(!$routeParams.query){
-            word = $rootScope.searchOptions.query;    
-            opt = $rootScope.searchOptions.opt; 
-            SearchOptions.setSearchOptions({'query':word, 'opt':opt});
+        var word = String();
+        var opt = String();
+
+        if (!$routeParams.query) {
+            word = $rootScope.searchOptions.query;
+            opt = $rootScope.searchOptions.opt;
+            SearchOptions.setSearchOptions({'query': word, 'opt': opt});
         } else {
             word = $routeParams.query;
             opt = $routeParams.opt;
-            
-            var searchKeyWords = new String();
 
-            if (word.search(',') != -1) {
+            var searchKeyWords = String();
+
+            if (word.search(',') !== -1) {
                 var topics = word.split(',');
                 for (var i = 0; i < topics.length; i++) {
                     searchKeyWords += topics[i];
@@ -114,14 +101,14 @@ mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$rout
             } else {
                 searchKeyWords = word;
             }
-            
-            if (searchKeyWords.search('/') != -1 ) {
+
+            if (searchKeyWords.search('/') !== -1) {
                 var topics = searchKeyWords.split('/');
                 searchKeyWords = "";
                 for (var i = 0; i < topics.length; i++) {
                     searchKeyWords += topics[i];
                 }
-            } 
+            }
             
             word = searchKeyWords;
 
@@ -136,11 +123,9 @@ mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$rout
             $rootScope.searchOptions.query = searchKeyWords;
             $rootScope.searchOptions.opt = opt;
             
-            //opt = 'And';
         }
 
         function initController() {
-            // initialize to page 1
             vm.setPage(1);
         }
 
@@ -156,35 +141,34 @@ mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$rout
             vm.items = vm.PDItems.slice(vm.pager.startIndex, vm.pager.endIndex + 1);
         }
 
-        this.searchTopic = function(topic) {
-            var searchKeyWords = new String();
+        this.searchTopic = function (topic) {
+            var searchKeyWords = String();
 
-            if (topic.search(',') != -1 ) {
+            if (topic.search(',') !== -1) {
                 var topics = topic.split(',');
                 for (var i = 0; i < topics.length; i++) {
                     searchKeyWords += topics[i];
                 }
             } else {
                 searchKeyWords = topic;
-            } 
-            
-            if (searchKeyWords.search('/') != -1) {
+            }
+
+            if (searchKeyWords.search('/') !== -1) {
                 var topics = searchKeyWords.split('/');
                 searchKeyWords = "";
                 for (var i = 0; i < topics.length; i++) {
                     searchKeyWords += topics[i];
                 }
-            } 
-            
+            }
+
             $rootScope.searchOptions.query = searchKeyWords;
-            MetaData.get({query: searchKeyWords, operator: opt}, 
+            MetaData.get({query: searchKeyWords, operator: opt},
                 function success(response) {
                     vm.PDItems = response.PDResults;
                     vm.totalMatches = vm.PDItems.length;
                     vm.query = word;
                     vm.opt = opt;
                     initController();
-                    //$scope.PDResults = response.PDResults;
 
                 },
                 function error(errorResponse) {
@@ -193,89 +177,86 @@ mudrodControllers.controller('metadataViewCtrl', ['$rootScope', '$scope', '$rout
             );
         }
 
-        MetaData.get({query: word, operator: opt}, 
-                function success(response) {
-                    vm.PDItems = response.PDResults;
-                    vm.totalMatches = vm.PDItems.length;
-                    vm.query = word;
-                    vm.opt = opt;
-                    initController();
-                    //$scope.PDResults = response.PDResults;
+        MetaData.get({query: word, operator: opt},
+            function success(response) {
+                vm.PDItems = response.PDResults;
+                vm.totalMatches = vm.PDItems.length;
+                vm.query = word;
+                vm.opt = opt;
+                initController();
 
-                },
-                function error(errorResponse) {
-                    console.log("Error:" + JSON.stringify(errorResponse));
-                }
+            },
+            function error(errorResponse) {
+                console.log("Error:" + JSON.stringify(errorResponse));
+            }
         );
     }]);
 
 mudrodControllers.controller('datasetViewCtrl', ['$rootScope', '$scope', '$routeParams', 'DatasetDetail', 'SearchOptions',
     function datasetViewCtrl($rootScope, $scope, $routeParams, DatasetDetail, SearchOptions) {
-        var shortname = $routeParams.shortname;   
-        
-        var query = new String();
-        var opt = new String();
-        if(!$routeParams.query){
-        	query = $rootScope.searchOptions.query;    
-            opt = $rootScope.searchOptions.opt; 
-            SearchOptions.setSearchOptions({'query':query, 'opt':opt});
+        var shortname = $routeParams.shortname;
+
+        var query = String();
+        var opt = String();
+        if (!$routeParams.query) {
+            query = $rootScope.searchOptions.query;
+            opt = $rootScope.searchOptions.opt;
+            SearchOptions.setSearchOptions({'query': query, 'opt': opt});
         } else {
-        	query = $routeParams.query;
+            query = $routeParams.query;
             opt = $routeParams.opt;
             $rootScope.searchOptions.query = query;
             $rootScope.searchOptions.opt = opt;
             SearchOptions.setSearchOptions({'query':query, 'opt':opt});
         }
-        
-        DatasetDetail.get({shortname: shortname}, 
-                function success(response) {
-                    var dataAccessUrls;
 
-                    $scope.dataset = response.PDResults[0];
-                    var dataAccessUrl = $scope.dataset['DatasetLocationPolicy-BasePath'];
-                    if(dataAccessUrl.search(',') != -1) {
-                        dataAccessUrls =  dataAccessUrl.split(',');
-                        $scope.ftpUrl = dataAccessUrls[0];
-                        $scope.httpsUrl = dataAccessUrls[1];    
-                        $scope.hideUrls = false;
-                        $scope.hideUrl = true;                
-                    } else {
-                        $scope.hideUrl = false;
-                        $scope.hideUrls = true;
-                    }
-                },
-                function error(errorResponse) {
-                    console.log("Error:" + JSON.stringify(errorResponse));
+        DatasetDetail.get({shortname: shortname},
+            function success(response) {
+                var dataAccessUrls;
+
+                $scope.dataset = response.PDResults[0];
+                var dataAccessUrl = $scope.dataset['DatasetLocationPolicy-BasePath'];
+                if (dataAccessUrl.search(',') !== -1) {
+                    dataAccessUrls = dataAccessUrl.split(',');
+                    $scope.ftpUrl = dataAccessUrls[0];
+                    $scope.httpsUrl = dataAccessUrls[1];
+                    $scope.hideUrls = false;
+                    $scope.hideUrl = true;
+                } else {
+                    $scope.hideUrl = false;
+                    $scope.hideUrls = true;
                 }
+            },
+            function error(errorResponse) {
+                console.log("Error:" + JSON.stringify(errorResponse));
+            }
         );
     }]);
 
 mudrodControllers.controller('hRecommendationCtrl', ['$scope', '$routeParams', 'HRecommendation',
     function hRecommendationCtrl($scope, $routeParams, HRecommendation) {
-        var shortname = $routeParams.shortname;   
-        HRecommendation.get({shortname: shortname}, 
-                function success(response) {
-                    //alert($scope.challenge.question);
-                    // console.log("Success:" + JSON.stringify(response));
-                    $scope.recommendationList = response.HybridRecommendationData.linked;
+        var shortname = $routeParams.shortname;
+        HRecommendation.get({shortname: shortname},
+            function success(response) {
+                $scope.recommendationList = response.HybridRecommendationData.linked;
 
-                },
-                function error(errorResponse) {
-                    console.log("Error:" + JSON.stringify(errorResponse));
-                }
+            },
+            function error(errorResponse) {
+                console.log("Error:" + JSON.stringify(errorResponse));
+            }
         );
     }]);
 
-mudrodControllers.controller('TabCtrl', ['$scope', 
-    function($scope) {
-    $scope.tab = 1;
+mudrodControllers.controller('TabCtrl', ['$scope',
+    function ($scope) {
+        $scope.tab = 1;
 
-    $scope.setTab = function(newTab){
-      $scope.tab = newTab;
-    };
+        $scope.setTab = function (newTab) {
+            $scope.tab = newTab;
+        };
 
-    $scope.isSet = function(tabNum){
-      return $scope.tab === tabNum;
-    };
-}]);
+        $scope.isSet = function (tabNum) {
+            return $scope.tab === tabNum;
+        };
+    }]);
 
